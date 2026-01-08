5bfaa5809499766f0656dc6b8ac4495a81caff72
Aditya <adityadaksh002@gmail.com>
2026-01-08 18:45:05 +0530

    feat: integrate Tailwind CSS and update App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Home from "./pages/Home";
+import SaleBar from "./components/SaleBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <SaleBar />
+      <div className="px-12 md:px-16 lg:px-24">
+        <Home />
+      </div>
+    </>
+  );
 }
 
 export default App;
